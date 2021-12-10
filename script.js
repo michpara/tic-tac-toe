cb2de71c81de136d4e421f538ff335e27fd78662
Michelle Paradis <46390671+michpara@users.noreply.github.com>
2021-12-10 15:00:41 -0500

    setting up javascript factory functions

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//script
+const GameBoard = () => {
+	let size = 3;
+	let gamebaord = new Array(size);
+	for(var i = 0; i<size; i++){
+		gameboard[i] = new Array(size);
+	}
+	const getGameBoard = () => gameboard;
+
+	return {getGameBoard}
+}
+
+const Player = (symbol) => {
+  const getSymbol = () => symbol;
+
+  return {getSymbol}
+};
+
+const Game = (player1, player2, gameboard) => {
+	
+}
