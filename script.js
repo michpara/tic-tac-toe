f1dd374f10cca30f1d84ceec64d25e57d9265450
Michelle Paradis <46390671+michpara@users.noreply.github.com>
2021-12-10 15:21:06 -0500

    make html display game board

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 const GameBoard = (() => {
 	let size = 3;
-	let gamebaord = new Array(size);
+	let gameboard = new Array(size);
 	for(var i = 0; i<size; i++){
 		gameboard[i] = new Array(size);
 	}
+
 	const getGameBoard = () => gameboard;
 
-	return {getGameBoard};
+	const displayGameBoard = () => {
+		let rows = gameboard.length;
+		let cols = gameboard[0].length;
+		let gameboardDiv = document.querySelector("#gameboard");
+		for(var row = 0; row < rows; row++){
+			for(var col = 0; col < cols; col++){
+				var cell = document.createElement("div");
+				cell.innerHTML = gameboard[col][row];
+				cell.setAttribute("class", "cell");
+				gameboardDiv.appendChild(cell);
+
+				cell.style.top = row * 110 + "px";
+				cell.style.left = col * 110 + "px";
+			}
+		}
+	}
+
+	return {getGameBoard, displayGameBoard};
 })();
 
 const Player = (symbol) => {
@@ -18,3 +36,4 @@ const Player = (symbol) => {
 const Game = ((player1, player2, gameboard) => {
 
 })();
+
